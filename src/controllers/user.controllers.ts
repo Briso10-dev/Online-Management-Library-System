e4dea8c3a0e7f529a65d9cdf4d6f9e7b0557a20b
Briso10 <Kouambrice10@gmail.com>
2024-07-29 02:19:54 +0100

    updateUser_profile

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,4 +1,4 @@
-import { Request,Response } from "express";
+import { Request, Response } from "express";
 import { HttpCode } from "../core/constants";
 import { PrismaClient } from "@prisma/client";
 import chalk from 'chalk'
@@ -11,71 +11,102 @@ const prisma = new PrismaClient() //orm creation
 
 export const userControlleurs = {
     // get user profile
-    getUser : async (req:Request, res:Response) =>{
-        const {id} = req.params
-        
+    getUser: async (req: Request, res: Response) => {
+        const { id } = req.params
+
         try {
             const userProfile = await prisma.user.findUnique({
                 where: {
-                    userID : id
+                    userID: id
                 }
             })
-           if (userProfile)
-            res.json(userProfile)
-           else res.json({msg:"User info's failed retrieval"}).status(HttpCode.NOT_FOUND)
+            if (userProfile)
+                res.json(userProfile)
+            else res.json({ msg: "User info's failed retrieval" }).status(HttpCode.NOT_FOUND)
 
-        }catch (error) {
-            sendError(res,error)
+        } catch (error) {
+            sendError(res, error)
         }
     },
-    createUser : async (req:Request,res:Response)=>{
-         // Check for validation errors
-         const errors = validationResult(req);
-         if (!errors.isEmpty()) 
-             return res.status(HttpCode.UNPROCESSABLE_ENTITY).json({ errors: errors.array() });
- 
+    createUser: async (req: Request, res: Response) => {
+        // Check for validation errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty())
+            return res.status(HttpCode.UNPROCESSABLE_ENTITY).json({ errors: errors.array() });
+
         try {
-            const {name,email,password} = req.body
+            const { name, email, password } = req.body
+
+            const passHash = await bcrypt.hash(password, 10)
+            const user = await prisma.user.create({
+                data: {
+                    name,
+                    email,
+                    password: passHash
+                }
+            })
+            if (user)
+                res.status(HttpCode.CREATED).json(user)
+            else
+                res.status(HttpCode.BAD_REQUEST).json({ msg: "User could not be created !" })
+        } catch (error) {
+            sendError(res, error)
+        }
+    },
+    loginUser: async (req: Request, res: Response) => {
+        try {
+            const { email, password } = req.body
+
+            const user = await prisma.user.findFirst({
+                where: {
+                    email
+                },
+            })
+            if (user) {
+                const testPass = await bcrypt.compare(password, user.password)
+                if (testPass) {
+                    // jwt token generation
+                    const accessToken = tokenOps.generateAccessToken(user)
+                    const refreshToken = tokenOps.generateRefreshToken(user)
+                    user.password = " "
+                    res.cookie(" User successfully connected", refreshToken, { httpOnly: true, secure: true }) //refresh token stored in cookie
+                    res.json({ msg: "User successfully logged in" }).status(HttpCode.OK)
+                } else res.json({ msg: "Invalid infos enterd" })
+            } else console.log(chalk.red("No user found"))
+
+        } catch (error) {
+            sendError(res, error)
+        }
+    },
+    // logoutUser: async (req: Request, res: Response) => {
+    //     try {
+    //         // destroying user token
+    //         res.cookie(user)
+    //     } catch (error) {
+    //         sendError(res, error)
+    //     }
+    // },
+    updateUser: async (req: Request, res: Response) => {
+        try {
+            const { id } = req.params //obtaining a user's id
+            const { name, email, password } = req.body //obtaining modified users's info
 
             const passHash = await bcrypt.hash(password,10)
-               const user = await prisma.user.create({
-                data : {
+
+            const updateUser = await prisma.user.update({
+                where:{
+                    userID : id
+                },
+                data:{
                     name,
                     email,
                     password : passHash
                 }
-               })
-               if(user)
-                    res.status(HttpCode.CREATED).json(user)
-               else
-                    res.status(HttpCode.BAD_REQUEST).json({msg:"User could not be created !"})            
-        } catch (error) {
-            sendError(res,error)
-        }
-    },
-    loginUser : async (req:Request,res:Response)=>{
-        try {
-            const {email,password} = req.body
-
-            const user = await prisma.user.findFirst({
-                where:{
-                    email
-                },
             })
-            if(user){
-                const testPass = await bcrypt.compare(password,user.password)
-                if(testPass){
-                    // jwt token generation
-                    const accessToken = tokenOps.generateAccessToken(user)
-                    const refreshToken= tokenOps.generateRefreshToken(user)
-                    user.password = " "
-                    res.cookie(" User successfully connected",refreshToken,{httpOnly: true, secure: true}) //refresh token stored in cookie
-                    res.json({ msg: "User successfully logged in" }).status(HttpCode.OK)
-                }else res.json({msg:"Invalid infos enterd"})
-            }else console.log(chalk.red("No user found"))
-
+                if (updateUser) res.status(HttpCode.OK).json({msg:"User succesfully updated"})
+                else res.status(HttpCode.BAD_REQUEST).json({msg:"enterd correct infos"})
         } catch (error) {
-            sendError(res,error)
+            sendError(res, error)
         }
     }
 }
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -14,6 +14,6 @@ userRoute.post("/login",userControlleurs.loginUser)
 // user deconnexion
 userRoute.post("/logout")
 // update user profile
-userRoute.put("/profile")
+userRoute.put("/profile/:id",userControlleurs.updateUser)
 // delete an acount user
 userRoute.delete("/profile")
